2ba43133cab95c4690220563ee0598723acad0e7
Paola Golombieski Ciarcia <paolaciarcia20@gmail.com>
2023-06-09 11:13:54 -0300

    Organize files and folders

--- Recipes&Cooking/Recipes&Cooking/RecipeForm/Model/Recipe.swift
+++ Recipes&Cooking/Recipes&Cooking/RecipeForm/Model/Recipe.swift
@@ -4,9 +4,9 @@ import Foundation
 import UIKit
 
 
-class Receita {
+class Recipe {
     var image : UIImage? {
-        Receita.loadImage(forRecipe: self)
+        Recipe.loadImage(forRecipe: self)
     }
     var name : String
     var timePrepare : Int?
@@ -25,14 +25,14 @@ class Receita {
         return !(name.isEmpty || ingredients.isEmpty || instructions.isEmpty || timePrepare == nil || serves == nil)
     }
     
-    static func saveImage(_ image: UIImage, forRecipe recipe: Receita) {
+    static func saveImage(_ image: UIImage, forRecipe recipe: Recipe) {
         let imageURL = FileManager.documentDirectoryURL.appendingPathComponent(recipe.name)
       if let jpgData = image.jpegData(compressionQuality: 0.7) {
         try? jpgData.write(to: imageURL, options: .atomicWrite)
       }
     }
     
-    static func loadImage(forRecipe recipe: Receita) -> UIImage? {
+    static func loadImage(forRecipe recipe: Recipe) -> UIImage? {
         let imageURL = FileManager.documentDirectoryURL.appendingPathComponent(recipe.name)
       return UIImage(contentsOfFile: imageURL.path)
     }
